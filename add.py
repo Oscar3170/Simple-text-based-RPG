34cf2d09d1db5bbdd8fda9c6fd158c7678ad21d3
Oscar3170 <oscar3170@gmail.com>
2016-06-12 19:25:16 -0300

    Xablau

--- add.py
+++ add.py
@@ -1,5 +1,7 @@
+from list_bag import bag
+
 def enemies_txt(area):
-	file=open('enemies.py', 'w')
+	file=open('list_enemies.py', 'w')
 	if area==0:
 		file.write('enemies=[\'Rat  03  10  35\', \'Pigeon  04  05  45\', \'Big Rat  04  20  65\', \'Amoeba  1  20  20\']')
 		file.close()
@@ -9,3 +11,11 @@ def enemies_txt(area):
 	else:
 		file.write('enemies=[]')
 		file.close()
+
+def items_txt(new_item):
+	_bag_=bag
+	_bag_.append(new_item)
+	file=open('list_bag.py', 'w')
+	file.write('bag=[\''+'\', \''.join(_bag_)+'\']')
+
+items_txt('Pinto  dmg  9999')
--- list_bag.py
+++ list_bag.py
@@ -1,1 +1,1 @@
-bag=['Bread  heal  10', 'Water  heal  5', 'Flames  dmg  20', 'X Defense  boost  defense  3', 'X Attack  boost  atk  2']
+bag=['Bread  heal  10', 'Water  heal  5', 'Flames  dmg  20', 'X Defense  boost  defense  3', 'X Attack  boost  atk  2', 'Pinto  dmg  9999']
