eebf0eec91d51566b987ac009f0f8de513b93e3f
Oscar3170 <oscar3170@gmail.com>
2016-06-12 20:05:21 -0300

    Xablau

--- add.py
+++ add.py
@@ -16,19 +16,26 @@ def items_txt_add(new_item):
 	_bag_=bag
 	_bag_.append(new_item)
 	file=open('list_bag.py', 'w')
-	file.write('bag=', _bag_)
+	file.write('bag=[\''+'\', \''.join(_bag_)+'\']')
 	file.close()
 
 def items_txt_rmv(item):
 	_bag_= bag
+	position=0
 	for the_item in _bag_:
 		if item == the_item.split('  ')[0]:
 			split_item=the_item.split('  ')
-		if split_item[1]=='boost':
-			split_item[4]=str(int(split_item[4])-1)
+		position+=1
+	if split_item[1]=='boost':
+		del split_item[4]
+		split_item.append(str(int(split_item[4])-1))
+	else:
+		#del split_item[3]
+		split_item[3]=str(int(split_item[3])-1)
+		if split_item=='0':
+			_bag_.remove(the_item)
 		else:
-			split_item[4]=str(int(split_item[3])-1)					 
-			if split_item=='0':
-				print('xablau')
+			joined_item='  '.join(split_item)
+			print (joined_item)
 
-
+items_txt_rmv('Bread')
